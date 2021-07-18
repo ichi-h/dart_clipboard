3542db715bf38bc0ea806274e0d91aebe8d54fbf
ippee <ippee.music@gmail.com>
2021-07-19 05:34:56 +0900

    Rebuild directory structure.

--- lib/src/platform_exception.dart
+++ lib/src/platform_exception.dart
@@ -2,6 +2,8 @@
 // This source code is under the MIT License.
 // See http://opensource.org/licenses/mit-license.php
 
+library dart_clipboard;
+
 /// An exception thrown when called from unsupported platforms.
 class PlatformException implements Exception {
   /// Create new instance of [PlatformException].
